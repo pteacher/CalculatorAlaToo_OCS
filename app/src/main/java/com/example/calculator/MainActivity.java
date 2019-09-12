dc68bf15688df637317b4af67d9f02ab94b217a4
pteacher <subfear@mail.ru>
2019-09-12 15:01:07 +0600

    Init

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    int selectedItem = 1;
+    EditText age = findViewById(R.id.age);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button calcButton = findViewById(R.id.calculate);
+        final TextView result = findViewById(R.id.result);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerActivity);
+        ArrayAdapter<CharSequence> array = ArrayAdapter.createFromResource(this, R.array.activity, android.R.layout.simple_spinner_item);
+        array.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(array);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = position + 1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText(Integer.toString(Integer.parseInt(age.getText().toString()) * selectedItem));
+            }
+        });
+
+    }
+}
